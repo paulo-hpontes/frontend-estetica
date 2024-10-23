1abda99cf6064774e1a3a6246545677ef9f9bd15
paulo-hpontes <pauloh.teles16@gmail.com>
2024-10-22 20:50:35 -0400

    First  change
    Initial change in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
+// CSS
+import './Home.css';
+import homeIMG from "../../assets/Home.png";
 
 const Home = () => {
   return (
-    <div>
-      teste
-    </div>
-  )
-}
-
+    <main>
+      <section id="home" className='container'>
+        <div>
+          <h1>Deseja se sentir mais bonita?</h1>
+          <p>Realce sua beleza de forma natural</p>
+          <button>Agendar</button>
+        </div>
+        <div>
+          <img src={homeIMG} alt="Imagem de apresentação" />
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Home;

4643c90216eaa3a4cedff00bd7b6347a21d478dd
paulo-hpontes <pauloh.teles16@gmail.com>
2024-10-23 15:04:00 -0400

    Navbar and footer
    created the structure of navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import './App.css'
-
+import './App.css';
 
 // React Hooks
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Components
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 // Pages
-import Home from './pages/Home/Home'
+import Home from './pages/Home/Home';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home/>}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )

d41d0a4050e30de6871d1de5ae2ac342029f474b
paulo-hpontes <pauloh.teles16@gmail.com>
2024-11-02 04:14:50 -0400

    First-Animation
    starting to do some animation on layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,49 @@
-import './App.css';
+import "./App.css";
 
 // Custom Hooks
-import { useAuth } from './hooks/useAuth';
+import { useAuth } from "./hooks/useAuth";
 
 // React Hooks
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 // Components
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 // Pages
-import Home from './pages/Home/Home';
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
-import Service from './pages/Service/Service';
-import Scheduling from './pages/Scheduling/Scheduling';
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Service from "./pages/Service/Service";
+import Scheduling from "./pages/Scheduling/Scheduling";
 
 function App() {
-  const {auth, loading} = useAuth();
-  
-  if(loading) {
-    return <p>Loading...</p>
+  const { auth, loading } = useAuth();
+
+  if (loading) {
+    return <p>Loading...</p>;
   }
   return (
     <>
       <BrowserRouter>
         <Navbar />
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/services' element={<Service/>} />
-            <Route path="/scheduling"  element={<Scheduling/>} />
-            <Route path="/login"  element={!auth ? <Login /> : <Navigate to='/'/>} />
-            <Route path="/register"  element={!auth ? <Register /> : <Navigate to='/'/>} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Service />} />
+          <Route path="/scheduling" element={<Scheduling />} />
+          <Route
+            path="/login"
+            element={!auth ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/register"
+            element={!auth ? <Register /> : <Navigate to="/" />}
+          />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

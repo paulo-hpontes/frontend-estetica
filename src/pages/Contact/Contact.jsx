13643641066dede0f4d896433565e21fac876521
paulo-hpontes <pauloh.teles16@gmail.com>
2024-11-11 18:16:49 -0400

    Reponsiveness
    Last change in page responsiveness

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,6 +6,7 @@ import { FaInstagramSquare } from "react-icons/fa";
 const Contact = () => {
   return (
     <section id="contact">
+      <div className="img"><img src="Logo.png" alt="logo do site Lash Designer" title="Logo" /></div>
       <div className="address">
         <div className="title">
           <h2>Onde estamos</h2>
@@ -15,7 +16,10 @@ const Contact = () => {
           <div>
             <h3>Endereço:</h3>
             <p>
-              <strong>Rua:</strong> Oscar Romero, 105 - Terra Nova, Manaus/Am
+              <strong>Rua:</strong> Oscar Romero, 105
+            </p>
+            <p>
+              <strong>Bairro:</strong> Terra Nova, Manaus/Am
             </p>
             <p>
             <strong>CEP:</strong> 69097-475
@@ -29,22 +33,22 @@ const Contact = () => {
               </a>
             </button>
           </div>
-          <img src="Logo.png" alt="logo do site Lash Designer" title="Logo" />
           <div className="contact-info">
             <h3>Contato:</h3>
             <p>
-              <BsFillTelephoneFill />
-              (92) 99459-7308
+              <BsFillTelephoneFill size={30} style={{"margin-right": ".5rem"}}/>
+              <strong>(92) 99459-7308</strong>
             </p>
-            <p>
+           
               <a
                 href="https://www.instagram.com/ranni.lashdesigner?igsh=a3J1ZnY5NHpxN3Mz"
                 target="_blank"
               >
-                <FaInstagramSquare />
+                <p>
+                  <FaInstagramSquare size={30} style={{"margin-right": ".5rem"}}/>instagram
+                </p>
               </a>
-              instagram
-            </p>
+            
           </div>
         </div>
       </div>
